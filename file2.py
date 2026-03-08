679a2eb341a5feca926d4f8cf37eb11e7d2c5844
Jyothirmai12346 <1972csraju@gmail.com>
2026-03-08 14:01:28 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,13 @@
+import requests
+url= "https://api.github.com/orgs/microsoft/repos"
+response=requests.get(url)
+repos=response.json()
+print(" Microsoft open source repositories")
+for repo in repos:
+  name = repo["name"]
+  description = repo["description"]
+  url = repo["url"]
+  print("Repository name :",name)
+  print("Description:",description)
+  print("url:",url)
+  print("/" * 10)
